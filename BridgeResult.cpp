28203a6f4e5a8f3938b824d64365edcef8ac45d1
Blockchain Help <31084431+blockchainhelp@users.noreply.github.com>
2018-09-15 09:29:35 +0530

    Add files via upload

---
+++ BridgeResult.cpp
@@ -0,0 +1,25 @@
+#include "BridgeResult.h"
+#include "Bridge.h"
+#include <QCoreApplication>
+
+BridgeResult::BridgeResult()
+{
+    EnterCriticalSection(&Bridge::getBridge()->csBridge);
+    ResetEvent(Bridge::getBridge()->hResultEvent);
+}
+
+BridgeResult::~BridgeResult()
+{
+    LeaveCriticalSection(&Bridge::getBridge()->csBridge);
+}
+
+dsint BridgeResult::Wait()
+{
+    //Don't freeze when waiting on the main thread (https://github.com/x64dbg/x64dbg/issues/1716)
+    if(GetCurrentThreadId() == Bridge::getBridge()->dwMainThreadId)
+        while(WaitForSingleObject(Bridge::getBridge()->hResultEvent, 10) == WAIT_TIMEOUT)
+            QCoreApplication::processEvents();
+    else
+        WaitForSingleObject(Bridge::getBridge()->hResultEvent, INFINITE);
+    return Bridge::getBridge()->bridgeResult;
+}
